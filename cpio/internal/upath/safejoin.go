ad8a728465d4157ec24aa14c52ca33e7edd27044
Chris Koch <chrisko@google.com>
2024-01-22 04:17:28 +0000

    Add upath package comment
    Signed-off-by: Chris Koch <chrisko@google.com>

--- cpio/internal/upath/safejoin.go
+++ cpio/internal/upath/safejoin.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package upath allows safely joining filepaths together without breaking the
+// boundary of a "chroot".
 package upath
 
 import (
