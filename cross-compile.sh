bde385547a01497c6a1b20d92466079d69664b7f
Daniel Maslowski <info@orangecms.org>
2026-04-13 15:39:07 +0200

    upgrade to latest gobusybox
    The move of gobusybox to Go 1.25 forces mkuimage to use it, too.
    This upgrade enables the open PR for adding the `-compiler` option:
    https://github.com/u-root/mkuimage/pull/34
    
    To get to 1.25, this also does:
    
    * cross-compile.sh: drop windows/arm
      This has been removed upstream, being broken since Go 1.18.
      https://github.com/golang/go/issues/71671
    * CI: bump to Go 1.25 and 1.26 actions for lint workflow
    * migrate golangci config
      This is the result of running `golangci-lint migrate` using v2.11.4.
      <https://golangci-lint.run/docs/product/migration-guide/>
      Note: exclusions have been reworked, and the migration dropped them.
      <https://github.com/golangci/golangci-lint/issues/5298>
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- cross-compile.sh
+++ cross-compile.sh
@@ -19,12 +19,16 @@ function buildem() {
   done
 }
 
+# TBD netbsd openbsd plan9
+
 GOARCHES="386 amd64 arm arm64 ppc64 ppc64le s390x mips mipsle mips64 mips64le"
 buildem "linux" "$GOARCHES"
 
-GOARCHES="386 amd64 arm arm64"
-GOOSES="freebsd windows" # TBD netbsd openbsd plan9
-buildem "$GOOSES" "$GOARCHES"
+GOARCHES="386 amd64 arm64"
+buildem "windows" "$GOARCHES"
 
-GOARCHES_DARWIN="arm64 amd64"
-buildem "darwin" "$GOARCHES_DARWIN"
+GOARCHES="386 amd64 arm arm64"
+buildem "freebsd" "$GOARCHES"
+
+GOARCHES="arm64 amd64"
+buildem "darwin" "$GOARCHES"
